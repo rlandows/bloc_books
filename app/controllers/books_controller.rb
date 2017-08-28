ed31b753e48bf970806250afc7745cb1b17d4781
rlandows <rlandows@uci.edu>
2017-08-28 00:40:33 -0700

    checkpoint and assignment 4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,18 +5,29 @@ class BooksController < BlocWorks::Controller
    end
 
    def index
-        render :index, books: Book.all
+        render books: Book.all
     end
 
+    # def show
+    #   # start a REPL session
+    #   # binding.pry
+    #
+    #   req = Rack::Request.new(@env)
+    #   puts "Here are the #{req.params}"
+    #   @name = req.params.key("show")
+    #   index = Book.find_position(@name)
+    #   @book = Book.all[index]
+    #   render :show, book: @book
+    # end
+
     def show
-      # start a REPL session
-      binding.pry
-      
-      req = Rack::Request.new(@env)
-      puts "Here are the #{req.params}"
-      @name = req.params.key("show")
-      index = Book.find_position(@name)
-      @book = Book.all[index]
-      render :show, book: @book
+    if @routing_params["id"] != "show"
+      book = Book.all[@routing_params["id"].to_i - 1]
+      render book: book
+    else
+       req = Rack::Request.new(@env)
+       book = Book.all[req.params['id'].to_i - 1]
+       render  book: book
     end
+   end
  end
--- config.ru
+++ config.ru
@@ -1,2 +1,14 @@
 require './config/application'
-run BlocBooks::Application.new
+app = BlocWorks::Application.new
+
+ use Rack::ContentType
+
+ app.route do
+  #  map "", "books#welcome"
+  #  map ":controller/:id/:action"
+  #  map ":controller/:id", default: { "action" => "show" }
+  #  map ":controller", default: { "action" => "index" }
+   resources :books
+ end
+
+ run(app)
